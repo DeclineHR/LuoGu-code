25547104b275c7ae74c4c80e00e5dce17a6f8e51
DeclineHR <hk2269392001@163.com>
2024-10-12 21:10:10 +0800

    Update 蓝桥萌3-2遗失的数字.cpp

--- 蓝桥萌3-2遗失的数字.cpp
+++ 蓝桥萌3-2遗失的数字.cpp
@@ -13,8 +13,8 @@ int main()
 
     set<long>b(p, p + N); //set函数实现去重，排序
     long len = b.size(); //计算set后的数组大小
-    long * arr = new long[len];//copy函数复制set后的元素进入数组 以进行binary_search
-    copy(b.begin(), b.end(),arr);
+    long * arr = new long[len];//创建动态数组arr来保存set里的元素
+    copy(b.begin(), b.end(),arr);//copy函数复制set后的元素进入数组 以进行binary_search
 
     for (int j = 0; j <= len; j++) {
         if (binary_search(arr, arr + len, r) == 1) {//binary_search来查找r
