0551fb3f5fef5e3e6c56e62395612bb8ee1a59d7
DeclineHR <hk2269392001@163.com>
2024-10-13 16:37:42 +0800

    Create 蓝桥萌3 手机靓号(待优化).cpp

---
+++ 蓝桥萌3 手机靓号(待优化).cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+  int main(){
+      int N;
+      int * p = new int[10000];
+      int count = 0;
+      cin >> N;
+      for (int k = 0; k < 10000; k++) {
+          p[k] = 0;
+      }
+      for (int i = 0; i < N; i++) {
+          string b;
+          cin >> b;
+          int len = b.size();
+
+
+          for (int j = 0; j < len; j++) {
+              if (b[0] == b[j]) {
+                  p[i]++;
+              }
+          }
+          if (p[i] == len) {
+              count++;
+          }
+      }
+
+  cout << count;
+  return 0;
+}
