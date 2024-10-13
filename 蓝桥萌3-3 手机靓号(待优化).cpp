926ac38857009769ff2eedaa18447d05c7cc4a2d
DeclineHR <hk2269392001@163.com>
2024-10-13 19:21:18 +0800

    Update 蓝桥萌3-3 手机靓号.cpp

--- 蓝桥萌3-3 手机靓号(待优化).cpp
+++ 蓝桥萌3-3 手机靓号(待优化).cpp
@@ -5,23 +5,27 @@ using namespace std;
       int * p = new int[10000];
       int count = 0;
       cin >> N;
-      for (int k = 0; k < 10000; k++) {
+     /* for (int k = 0; k < 10000; k++) {
           p[k] = 0;
       }
+    */
+      //使用fill()函数优化
+    fill(p,p + 10000,0);
       for (int i = 0; i < N; i++) {
           string b;
           cin >> b;
           int len = b.size();
-
 
           for (int j = 0; j < len; j++) {
               if (b[0] == b[j]) {
                   p[i]++;
               }
           }
+        
           if (p[i] == len) {
               count++;
           }
+        
       }
 
   cout << count;
