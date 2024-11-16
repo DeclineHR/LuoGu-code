dcd28be9379454ec1a055a4d28ed1b4b42a7245f
DeclineHR <hk2269392001@163.com>
2024-11-16 21:18:01 +0800

    Update A - B (高精度).cpp

--- A - B (高精度).cpp
+++ A - B (高精度).cpp
@@ -1,5 +1,6 @@
 https://www.bilibili.com/video/BV1kb4y1A7Wf?spm_id_from=333.788.videopod.sections&vd_source=02f6e5314cea9dd0571654ef8f035d6b
 #include<iostream>
+#include<algorithm>
 #include<string>
 using namespace std;
 int main() {
@@ -14,13 +15,14 @@ int main() {
 		swap(s1, s2);//确保s1>s2
 		fuhao = '-';
 	}
-	for (int i = 0; i < s1.size(); i++) {
-		a1[i] = s1[s1.size() - i - 1] - '0';
+	for (int i = 1; i <= s1.size(); i++) {
+		a1[i] = s1[s1.size() - i] - '0';//字符串也是按照0开始排
+		//si.size()输出的是元素的个数   si.size() - i代表倒序赋值
 	}
-	for (int j = 0; j < s1.size(); j++) {
-		a2[j] = s2[s2.size() - j - 1] - '0';
+	for (int j = 1; j <= s1.size(); j++) {
+		a2[j] = s2[s2.size() - j] - '0';
 	}
-	for (int i = 0; i < s1.size(); i++) {
+	for (int i = 1; i <= s1.size(); i++) {
 		if (a1[i] < a2[i]) {//位数不够要借位
 			a1[i] += 10;
 			a1[i + 1] -= 1;
@@ -28,7 +30,7 @@ int main() {
 		r[i] = a1[i] - a2[i];
 	}
 	int index = 0;
-	for (int i = s1.size(); i >= 0; i--) {
+	for (int i = s1.size(); i > 0; i--) {
 		if (r[i] != 0) {
 			index = i;
 			break;
@@ -37,7 +39,7 @@ int main() {
 	if (fuhao == '-') {
 		cout << fuhao;
 	}
-	for (int j = index; j >= 0; j--) {
+	for (int j = index; j > 0; j--) {
 		cout << r[j];
 	}
 	return 0;
